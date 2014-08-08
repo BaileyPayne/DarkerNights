3c737e48cfa04c41f232ab01a0b5132d7b558c89
Bailey Payne <Bailpy6@aol.co.uk>
2014-08-08 21:06:21 +0100

    Checked for Light levels rather then day or night.

--- src/me/baileypayne/darkernights/DarkerNights.java
+++ src/me/baileypayne/darkernights/DarkerNights.java
@@ -1,9 +1,12 @@
 
 package me.baileypayne.darkernights;
 
+import org.bukkit.Location;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -18,21 +21,16 @@ public class DarkerNights extends JavaPlugin implements Listener {
     public void onEnable(){
         
     }
-    //check if night
-    public boolean day() {
-    Server server = getServer();
-    long time = server.getWorld("world").getTime();
-    if(time > 0 || time < 12300){
-        return true;
-    }
-        return false;
-    }
-    public void darkNights(Player player, boolean day){
-        if(day == false){
-            player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, Integer.MAX_VALUE, 1));
+    @EventHandler
+    public void onPlayerMoveEvent(PlayerMoveEvent e){
+        Player p = e.getPlayer();
+        Location l = p.getLocation();
+        int lightlevel = p.getLocation().getBlock().getLightLevel();
+        if(lightlevel < 5){
+            p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, Integer.MAX_VALUE, 1));
         }
-        if(day == true){
-            player.removePotionEffect(PotionEffectType.BLINDNESS);
+        else {
+            p.removePotionEffect(PotionEffectType.BLINDNESS);
         }
     }
     
