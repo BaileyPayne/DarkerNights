5a23e287ffec40a9d68f44f3e6e1aaa42a81e77e
Bailey Payne <Bailpy6@aol.co.uk>
2014-08-10 20:22:28 +0100

    Small Changes - waiting to add a scheduler!!

--- src/me/baileypayne/darkernights/DarkerNights.java
+++ src/me/baileypayne/darkernights/DarkerNights.java
@@ -19,6 +19,7 @@ public class DarkerNights extends JavaPlugin implements Listener {
     
     @Override
     public void onEnable(){
+        getServer().getPluginManager().registerEvents(this, this);
         
     }
     @EventHandler
